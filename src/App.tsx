16f406923b65fe36f0fd54530bf2ca30a1cd2132
sky00069123 <sky00069123@gmail.com>
2025-09-18 01:27:49 +0100

    set default theme to light

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider defaultTheme="dark" storageKey="teemo-ui-theme">
+    <ThemeProvider defaultTheme="light" storageKey="teemo-ui-theme">
       <TooltipProvider>
         <Toaster />
         <Sonner />
